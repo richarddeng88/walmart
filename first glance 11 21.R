ce885ceb93e87ffffb8630328d083b657bb78658
Richard <richarddeng88@gmail.com>
2015-11-21 22:40:02 -0500

    first glance

---
+++ first glance 11 21.R
@@ -0,0 +1,21 @@
+train <- read.csv("data/walmart/train.csv")
+test <- read.csv("data/walmart/test.csv")
+sample <- read.csv("data/walmart/sample_submission.csv")
+
+sapply(train, function(x){sum(is.na(x))})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
