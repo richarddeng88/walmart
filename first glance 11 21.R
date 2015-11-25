c1162e21c990a9cae25c5919e893a18b5f550905
Richard <richarddeng88@gmail.com>
2015-11-24 22:50:05 -0500

    11 24

--- first glance 11 21.R
+++ first glance 11 21.R
@@ -5,7 +5,9 @@ sample <- read.csv("data/walmart/sample_submission.csv")
 sapply(train, function(x){sum(is.na(x))})
 table(train$TripType)
 
-
+## classification problem.   
+# a lot of NAs, so few predictors and quite a lot of types. 
+# catigoritize the departmentDescription use tree, CV, SVM, and 
 
 
 
